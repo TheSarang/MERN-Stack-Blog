a02321504ea3886b5c00f906d49859879259a551
Sarang Narkhede <sn7330@rit.edu>
2020-08-11 15:16:19 -0400

    first commit of blog

---
+++ src/build/precache-manifest.9c59d316714a4643199e85c76100fae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d84324833ab7379dcea5b500e1c799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37f484c7c7f5f36c673",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "bf2696f123afb4df0b93",
+    "url": "/static/js/2.b0a93b6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0a93b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37f484c7c7f5f36c673",
+    "url": "/static/js/main.c8f359c4.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
