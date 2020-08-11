c9caa65566ab17f6deed8077e429e772125f60a4
Sarang Narkhede <sn7330@rit.edu>
2020-08-11 16:14:19 -0400

    fixing bugs

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { MongoClient } from "mongodb";
 import path from "path";
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/build")));
@@ -72,6 +72,6 @@ app.post("/api/articles/:name/add-comment", (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
 });
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Listening on port");
 });
